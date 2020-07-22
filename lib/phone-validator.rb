ed3ca47138866bb7dfc351ee32ad893af473d8b4
vesh95 <vesh95.17@ya.ru>
2020-07-22 21:58:24 +0500

    Added country code

--- lib/phone-validator.rb
+++ lib/phone-validator.rb
@@ -1,12 +1,10 @@
 class PhoneValidator < ActiveModel::EachValidator
-  # validate 10 digit numbers
+  # validate 10 digit numbers with country code
   # optionally seperated by dashes
-  # 123-456-7890
-  # 1234567890
 
   def validate_each(record, attribute, value)
-    unless value =~ /^[0-9]{3}-?[0-9]{3}-?[0-9]{4}$/
-      record.errors[attribute] << (options[:message] || "is not valid") 
+    unless value =~ /^\+{1}[1-9]{1,4}-?[0-9]{3}-?[0-9]{3}-?[0-9]{4}$/
+      record.errors[attribute] << (options[:message] || "is not valid")
     end
   end
 end
--- test/test_phone-validator.rb
+++ test/test_phone-validator.rb
@@ -12,16 +12,24 @@ class TestPhoneValidator < Test::Unit::TestCase
       @phone = phone
     end
   end
-  
-  
+
+
   def test_valid_numbers
-    ['123-123-1234', '1234567890'].each do |number|
+    [
+      '+7-902-423-4233', '+5-332-523-4452',
+      '+5-332523-4452', '+53325234452', '+53223325234452',
+      '+5322-332-523-4452'
+    ].each do |number|
       assert Person.new(number).valid?
     end
   end
 
   def test_invalid_numbers
-    [123, '123456789', '123--456-7890', '12345678x1'].each do |number|
+    [
+      123,'123456789', '123--456-7890', '12345678x1', '123-123-1234',
+      '1234567890', '+0-332523-4452', '+-332523-4452', '+532823325234452',
+      '+53228-332-523-4452'
+    ].each do |number|
       assert !Person.new(number).valid?
     end
   end
